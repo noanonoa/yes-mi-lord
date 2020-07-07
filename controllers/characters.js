6be59db96b3be15707472c6c5783f93f261a7f41
Noa Kim <noanonoa@gmail.com>
2020-07-07 15:37:52 -0700

    show page, route, & random quotes implemented

--- controllers/characters.js
+++ controllers/characters.js
@@ -8,22 +8,45 @@ const paginate = require('express-paginate');
 
 router.use(paginate.middleware(100, 100));
 
+const randomInt = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+const token = {headers: {
+    "Authorization": `Bearer ${process.env.API_KEY}`
+}}
+
 router.get('/', (req, res) => {
-    axios.get('https://the-one-api.herokuapp.com/v1/character', {
-        headers: {
-            "Authorization": `Bearer ${process.env.API_KEY}`
-        }
-
-    }).then((results) => {
+    axios.get('https://the-one-api.herokuapp.com/v1/character', token)
+    
+    .then((results) => {
         
-
         res.render('characters', {
-            characters: results.data.docs,
-        
+            characters: results.data.docs
         })
     })
 })
 
+router.get('/:id', (req, res) => {
+    let characterId = req.params.id
+    axios.get('https://the-one-api.herokuapp.com/v1/character/' + characterId, token)
+    
+    .then((results) => {
+        let characterResponse = results.data
+        axios.get(`https://the-one-api.herokuapp.com/v1/character/${characterId}/quote`, token)
+
+        .then((results) => {
+            let characterQuotes = results.data.docs;
+            randomQuote = characterQuotes[randomInt(0, characterQuotes.length - 1)].dialog;
+
+
+            res.render('show', {
+                character: characterResponse,
+                randomQuote,
+            })
+        })
+    }).catch(err => console.log(err))
+
+})
 
 
 module.exports = router;
